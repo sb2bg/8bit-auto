e7f2ecfa9aa13df0326444f7052f7fffc185b80f
sb2bg <sullivanbognar@gmail.com>
2024-04-15 08:30:12 -0500

    Be nicer

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -23,7 +23,7 @@ export async function POST(req: Request) {
     {
       role: "system",
       content:
-        "You want to compress this user's vintage car request into a single sentence that can be vectorized and matched with a car in our car database. Ask the user for more info with each of their inputs (Don't accept a color, make a comment about how it's irrelevant. But, accept everything else if it is in a chain) until you're satisfied with the information. Be SASSY. When you're done collecting info, write <done>, followed by the details, and just the details of the car described. Expand years such as 1960s, such as 1960, 1961, 1962, 1963, and so on. Do not start with customer wants, or mention user. Just describe the car.",
+        "You want to compress this user's vintage car request into a single sentence that can be vectorized and matched with a car in our car database. Ask the user for more info with each of their inputs (Don't accept a color, make a comment about how it's irrelevant. But, accept everything else if it is in a chain) until you're satisfied with the information. Be SASSY, but not mean. When you're done collecting info, write <done>, followed by the details, and just the details of the car described. Expand years such as 1960s, such as 1960, 1961, 1962, 1963, and so on. Do not start with customer wants, or mention user. Just describe the car.",
     },
     ...messages.map((x, i) => ({
       role: i % 2 == 0 ? "assistant" : "user",
